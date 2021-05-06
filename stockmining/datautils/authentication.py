78b16562044420495427ed58d7cb44681331570d
jorseph <jorseph0418@msn.com>
2021-05-06 16:42:10 +0800

    Add some test function...

--- stockmining/datautils/authentication.py
+++ stockmining/datautils/authentication.py
@@ -6,23 +6,69 @@ More details in https://finmind.github.io/quickstart/.
 """
 
 import requests
+import pandas as pd
 
 URLLOGIN = "https://api.finmindtrade.com/api/v4/login"
+URLDATA = "https://api.finmindtrade.com/api/v4/data"
 
 
 def get_token(max_try=10):
     """Get the token."""
-
-    user_id = input('Enter the user id:')
-    user_password = input('Enter the user passward:')
-    parload = {"user_id": user_id, "password": user_password}
+    parload = {
+        "user_id": "jorseph",
+        "password": "yoyo0418",
+    }
+ 
     for _ in range(max_try):
         data = requests.post(URLLOGIN, data=parload).json()
         if data['msg'] == "success":
             return data['token']
     raise Exception("Faild to get token.")
 
+def get_stock_price(stock_id, token):
+    parameter = {
+        "dataset": "TaiwanStockPrice",
+        "data_id": stock_id,
+        "start_date": "2021-04-02",
+        "end_date": "2021-04-12",
+        "token": token, # 參考登入，獲取金鑰
+    }
+    resp = requests.get(URLDATA, params=parameter)
+    data = resp.json()
+    data = pd.DataFrame(data["data"])
+    print(data.head())
+    return data
+    
+#TAIEX is not suitable.
+def get_institutional_buy_sell(stock_id, token):
+    parameter = {
+        "dataset": "TaiwanStockInstitutionalInvestorsBuySell",
+        "data_id": stock_id,
+        "start_date": "2021-04-01",
+        "token": token, # 參考登入，獲取金鑰
+    }
+    resp = requests.get(URLDATA, params=parameter)
+    data = resp.json()
+    data = pd.DataFrame(data['data'])
+    print(data.head())
+    
+def get_all_buy_sell(token): 
+    parameter = {
+        "dataset": "TaiwanStockTotalInstitutionalInvestors",
+        "start_date": "2021-04-01",
+        "token": token, # 參考登入，獲取金鑰
+    }
+    resp = requests.get(URLDATA, params=parameter)
+    data = resp.json()
+    data = pd.DataFrame(data['data'])
+    print(data.head())
+    return data
 
 if __name__ == "__main__":
+    stock_id = 'TAIEX'
     token = get_token(max_try=10)
-    print(token)
+    #print(token)
+    price = get_stock_price(stock_id, token)
+    buysell = get_institutional_buy_sell(stock_id, token)
+    total_buy_sell = get_all_buy_sell(token)
+    
