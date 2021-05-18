e95075c1ca6474c3ef53561a704b12988974893c
jorseph <jorseph0418@msn.com>
2021-05-19 00:10:31 +0800

    WIP

--- stockmining/datautils/data_generator.py
+++ stockmining/datautils/data_generator.py
@@ -2,10 +2,13 @@
 """
 Created on Tue May 18 23:46:15 2021
 
-@author: jorse
+@author: jorseph
 """
 class DataGenerator:
-    def __init__(self, company_code, data_path='./stock_history', output_path='./outputs', strategy_type='original',
-                 update=False, logger: Logger = None):
-        self.company_code = company_code
+    def __init__(self, stock_code):
+        self.stock_code = stock_code
         self.df = self.create_features()
+    
+    def create_features(self):
+        
+
